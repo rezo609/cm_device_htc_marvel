f737cc1d86f4f30c7294e0bf67565bf59f3febaa
Benjamin Gwynn <13gwynn@gmail.com>
2012-07-28 17:17:27 +0100

    Fixed some lunch errors.

--- full_marvel.mk
+++ full_marvel.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/htc/marvel/device_marvel.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
-PRODUCT_NAME := aokp_marvel
+PRODUCT_NAME := cm_marvel
 PRODUCT_BRAND := htc_europe
 PRODUCT_MODEL := Wildfire S
 PRODUCT_MANUFACTURER := HTC
@@ -25,7 +25,7 @@ PRODUCT_VERSION_DEVICE_SPECIFIC := -CYANOGENMOD-10-NIGHTLY-$(NOW)
 CM_BUILDTYPE := NIGHTLY
 CM_EXTRAVERSION := CM-10-NIGHTLY-$(NOW)
 
-BUILD_ID := BENJAMINGWYNN-AOKP-$(NOW)
+BUILD_ID := BENJAMINGWYNN-CM-$(NOW)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cm/htc_marvel/marvel:4.1/cm$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="0.1 benjamingwynn release-keys"
